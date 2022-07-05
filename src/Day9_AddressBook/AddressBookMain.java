0aa6e8e709191998f4d9e7088306d306a6b31399
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-07-05 20:41:43 +0530

    create a Contacts in Address Book

---
+++ src/Day9_AddressBook/AddressBookMain.java
@@ -0,0 +1,29 @@
+package Day9_AddressBook;
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("First Name    : ");
+       String first_name = scanner.nextLine();
+        System.out.print("Last Name     : ");
+        String last_name = scanner.nextLine();
+        System.out.print("Address       : ");
+        String address = scanner.nextLine();
+        System.out.print("City Name     : ");
+        String city = scanner.nextLine();
+        System.out.print("State Name    : ");
+        String state = scanner.nextLine();
+        System.out.print("Zip Code      : ");
+        int zip = scanner.nextInt();
+        System.out.print("Phone NO      : ");
+        String phone = scanner.next();
+        System.out.print("Email Id      : ");
+        String email = scanner.next();
+
+        System.out.println("First Name    : "+first_name+"\nLast Name     : "+last_name+"\nAddress       : "+address+"\nCity Name     : "+city+"\nState Name    : "+state+"\nZip Code      : "+zip+"\nPhone NO      : "+phone+"\nEmail Id      : "+email);
+        System.out.println();
+    }
+}
