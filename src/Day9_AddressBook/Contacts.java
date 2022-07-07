854df8a0cbfdf96ff37aa54fbe7241259fc91e77
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-07-07 12:40:45 +0530

    Add contacts in address book

--- src/Day9_AddressBook/Contacts.java
+++ src/Day9_AddressBook/Contacts.java
@@ -1,33 +1,60 @@
 package Day9_AddressBook;
-
-import java.util.Scanner;
-
 public class Contacts {
-    public static void main(String[] args) {
+    private String first_name;
+        String last_name;
+        String address;
+        String city;
+        String state;
+        String zip;
+        String phone;
+        String email;
 
-        System.out.println("Welcome to Address Book Program");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("How many entries do you want in a book: ");
-        int entries = sc.nextInt();
-
-        AddressBookMain person = new AddressBookMain();
-        for (int i = 1; entries >= i; i++) person.Address();
-
-        System.out.print("Enter 1 to Edit\n Enter 2 to Delete :\n");
-        int ch = sc.nextInt();
-
-        switch (ch){
-            case 1:
-                System.out.print("Please enter name you want to edit: ");
-                String editname = sc.next();
-                add.search(name);
-                break;
-            case 2:
-                System.out.print("Please enter name you want to edit: ");
-                String deletename = sc.next();
-                add.search(name);
-                add.remove(name);
-                break;
-        }
+        public String getFirstName() {
+        return first_name;
+    }
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getLastName() {
+        return last_name;
+    }
+    public void setLastName(String lastName) {
+        this.last_name = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phone;
+    }
+    public void setPhoneNumber(String phone) {
+        this.phone = phone;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
