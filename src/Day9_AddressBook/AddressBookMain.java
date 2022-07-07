d22d97c7962ae7c36bffcc24207ee08f991d08fa
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-07-07 20:54:16 +0530

    Store contacts in address book

--- src/Day9_AddressBook/AddressBookMain.java
+++ src/Day9_AddressBook/AddressBookMain.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class AddressBookMain {
    static Scanner scanner = new Scanner(System.in);
-    List<Contacts> ContactsArrayList = new ArrayList<>();
+    List <Contacts> ContactsArrayList = new ArrayList<>();
     public void Address() {
         Contacts contact = new Contacts();
         System.out.println("******* New Contact *******");
@@ -34,12 +34,37 @@ public class AddressBookMain {
         System.out.print("Email Id      : ");
         String email = scanner.next();
         contact.setEmail(email);
-        System.out.println("First Name    : "+first_name+"\nLast Name     : "+last_name+"\nAddress       : "+address+"\nCity Name     : "+city+"\nState Name    : "+state+"\nZip Code      : "+zip+"\nPhone NO      : "+phone+"\nEmail Id      : "+email);
+//        System.out.println("First Name    : "+first_name+"\nLast Name     : "+last_name+"\nAddress       : "+address+"\nCity Name     : "+city+"\nState Name    : "+state+"\nZip Code      : "+zip+"\nPhone NO      : "+phone+"\nEmail Id      : "+email);
+
+        ContactsArrayList.add(contact);
+        System.out.println(contact.toString());
+    }
+
+    public void editContact()
+    {
+        System.out.println("Enter the first name of the contact you want to edit: ");
+        String firstName = scanner.next();
+        boolean flag = false;
+        for (Contacts contact : ContactsArrayList) {
+
+            if (firstName.equals(contact.getFirstName())) {
+                System.out.print("Enter new name: ");
+                String name = scanner.next();
+                contact.setFirstName(name);
+                System.out.println("Contact edited successfully!");
+                flag = true;
+                break;
+            }
+        }
+        System.out.print(ContactsArrayList);
+        if (!flag) {
+            System.out.println("The contact with first name : " + firstName + " is not found!");
+        }
     }
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Address Book");
-        AddressBookMain addressbook=new AddressBookMain();
+        System.out.println("\nWelcome to Address Book\n");
+        AddressBookMain addressbook = new AddressBookMain();
         addressbook.Address();
         System.out.println("Do you want to add contacts y/n");
         String status =scanner.next();
@@ -48,5 +73,11 @@ public class AddressBookMain {
         } else {
             System.out.println("Thank you!");
         }
+        System.out.println("Do you want to edit contacts y/n");
+        String edit = scanner.next();
+        if ((edit.equals("y"))){
+            addressbook.editContact();
+        }else
+            System.out.println("Thank You!!");
     }
 }
