f85f7c211b7263d6812ab2250e5e780f04163ae0
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-07-23 18:30:51 +0530

    Added: Added: Ability to view Persons by City or State

--- src/Day9_AddressBook/Contacts.java
+++ src/Day9_AddressBook/Contacts.java
@@ -1,60 +1,84 @@
 package Day9_AddressBook;
-public class Contacts {
-        String first_name ,last_name, address, city,state,zip,phone,email;
-
-    public String getFirstName() {
-        return first_name;
-    }
-    public void setFirstName(String first_name) {
-        this.first_name = first_name;
-    }
-    public String getLastName() {
-        return last_name;
-    }
-    public void setLastName(String lastName) {
-        this.last_name = lastName;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
+class Contacts {
+    private String firstName, lastName, address, city, state, email,zip,phoneNumber;
+    public Contacts (String firstName, String lastName, String address, String city, String state, String zip,
+                     String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
         this.email = email;
     }
-    public String getPhoneNumber() {
-        return phone;
+
+    public String getFirstName() {
+        return firstName;
     }
-    public void setPhoneNumber(String phone) {
-        this.phone = phone;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
-    public String getZip() {
-        return zip;
+
+    public String getLastName() {
+        return lastName;
     }
-    public void setZip(String zip) {
-        this.zip = zip;
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getState() {
-        return state;
-    }
-    public void setState(String state) {
-        this.state = state;
-    }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
-    //to string
 
-    @Override
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public String toString() {
-        return ("First name: " + first_name + "\n Last name: " + last_name + "\n Address: " + address + "\n city: " + city
-                + "\n state: " +state + "\n zip: " + zip + "\n phone number:" + phone + "\n email: " + email + "");
+        return ("First name: " + firstName + " Last name: " + lastName + " Address: " + address + " city: " + city
+                + " state: " + state + " email: " + email + " zip: " + zip + " phone number:" + phoneNumber + "");
     }
 }
