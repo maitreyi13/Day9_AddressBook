4bf390a090e4d2da988a32e9b813aade8dbeaaa6
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-07-30 14:38:07 +0530

    Delete AddressBookMain.java

--- AddressBookMain.java
+++
@@ -1,9 +0,0 @@
-package Day9_AddressBook;
-public class AddressBookMain extends Operations{
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Address book System project: ");
-        Operations operations = new Operations();
-        System.out.println("--------------------------Address Book-------------------------");
-        AddressBook(operations);
-    }
-}
